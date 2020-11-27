c5e12795fc7eaaeaa4915ab72d81b57e486915a7
Travis Persson <travispersson93@gmail.com>
2020-11-27 10:31:50 +0100

    init docker container with falsk

--- api/app.py
+++ api/app.py
@@ -1,1 +1,16 @@
-from Flask import Flask
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "testing testing"
+
+@app.route('/api')
+def api_call():
+    return jsonify({'status': 'is flask api working??'})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.getenv('PORT'))
