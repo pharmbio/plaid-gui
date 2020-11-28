0b37e51487de8d24cfdd4d5671f9e13867997c57
MarkusLucero <markuslucero@hotmail.com>
2020-11-28 16:34:41 +0100

    functionality to create .dzn files using pandas, json files are not supported yet

--- api/app.py
+++ api/app.py
@@ -1,7 +1,6 @@
 import os
 import csv
 import minizinc_model
-import minizinc
 import utils
 from flask import Flask, jsonify
 
@@ -23,8 +22,9 @@ def try_plaid():
     mz = minizinc_model.MinizincModel("./plate_design/plate-design.mzn", "gecode")
     mz.populate_instance(dzn_file_path="./plate_design/dzn_examples/pl-example06.dzn")
     result = mz.solve_instance()
-    print(result)
-    return result.solution["_output_item"]
+    mz.create_csv(result)
+    mz.create_json(result)
+    return str(result)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=os.getenv("PORT"))
--- api/utils.py
+++ api/utils.py
@@ -1,11 +1,1 @@
 import subprocess
-def main():
-    print("Hello!")
-    parse_output()
-def parse_output():
-    result = subprocess.run(["Hiiiii"], stdout=subprocess.PIPE)
-    print(result.stdout.decode('UTF-8'))
-
-
-if __name__ == "__main__":
-    main()
