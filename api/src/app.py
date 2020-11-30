d76e7d14f7c573d356455f9f9488b517235088e2
MarkusLucero <markuslucero@hotmail.com>
2020-11-30 19:25:46 +0100

    Basic API interface implemented for GET and POST requests. POST supports reading json payload

--- api/src/app.py
+++ api/src/app.py
@@ -1,23 +1,20 @@
 import os
 import minizinc_model
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import pandas as pd
 from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
 cors= CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "hello world!"
-
-@app.route("/plaid", methods=['GET'])
+@app.route("/", methods=['POST', 'GET'])
 def test_plaid():
-    mz = minizinc_model.MinizincModel("./plate_design/plate-design.mzn", "gecode")
-    mz.populate_instance(dzn_file_path="./plate_design/dzn_examples/pl-example06.dzn")
-    result = mz.solve_instance()
-    j_res = mz.output_to_json(result)
-    return j_res
+    if request.method  == 'GET':
+        return 'Hello!'
+    if request.method == 'POST':
+        data = request.get_json()
+    return data
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=os.getenv("PORT"))
