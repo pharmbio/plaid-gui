86ba26664f1468b3e3bfbab2ab7cf12988b9f8b8
MarkusLucero <markuslucero@hotmail.com>
2020-12-02 01:52:17 +0100

    added mmultiple input support for forms

--- client/src/components/ExperimentForm/ExperimentForm.js
+++ client/src/components/ExperimentForm/ExperimentForm.js
@@ -1,32 +1,28 @@
 import React, { useState } from "react";
 
-
-const ExperimentForm = () => {
-
-    return (
-        <form>
-            <label>
-                Plate rows:
-              <input type="text" />
-            </label>
-            <label>
-                Plate columns:
-              <input type="text" />
-            </label>
-            <label>
-                Vertical:
+const ExperimentForm = ({formState, handleInputChange }) => {
+    return (<>
+        <label>
+            Plate rows: 
+              <input type="number" name='rows' onChange={handleInputChange} />
+        </label>
+        <label>
+            Plate columns:
+              <input type="number" name='columns' onChange={handleInputChange} />
+        </label>
+        <label>
+            Vertical:
               <input
-                    name="isGoing" type="checkbox"
-                />
-            </label>
-            <label>
-                Horizontal:
+                name="isGoing" name='vertical' type="checkbox" onChange={handleInputChange}
+            />
+        </label>
+        <label> 
+            Horizontal:
               <input
-                    name="isGoing" type="checkbox"
-                />
-            </label>
-            <input type="submit" value="Submit" />
-        </form>
+                name="isGoing" name='horizontal' type="checkbox" onChange={handleInputChange}
+            />
+        </label>
+    </>
     )
 
 }
--- client/src/components/PlaidForm/PlaidForm.js
+++ client/src/components/PlaidForm/PlaidForm.js
@@ -3,11 +3,27 @@ import ExperimentForm from "../ExperimentForm/ExperimentForm"
 
 
 
-
-
 const PlaidForm = () => {
+    const [formState, setFormState] = useState({
+        rows: '',
+        columns: '',
+        vertical: false,
+        horizontal: false,
+    })
+    const handleInputChange = (event) => {
+        console.log(formState)
+        console.log(event.target.checked)
+        const target = event.target;
+        const value = event.target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        setFormState(({
+            ...formState,
+            [name]: value
+        }));
+    }
 
-    return (<div><ExperimentForm /></div>)
+
+    return (<form><ExperimentForm formState = {formState} handleInputChange={handleInputChange} /></form>)
 }
 
 export default PlaidForm
