4cdd90015b69aeaf84154205e448ab1331250c4a
Travis Persson <travispersson93@gmail.com>
2020-12-04 22:42:39 +0100

    experimenting with layout using styled components

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,47 +1,40 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const SideBar = styled.nav`
+  background-color: #584b4f;
+  height: 100vh;
+`;
+const LinkItem = styled.li`
+  list-style: none;
+`;
+const NavLink = styled(Link)`
+  color: #efeae1;
+  text-decoration: none;
+`;
 
 const Navbar = () => {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/instructions">Instructions</Link>
-          </li>
-          <li>
-            <Link to="/tool">Tool</Link>
-          </li>
-          <li>
-            <Link to="/publications">Publications</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route path="/instructions">
-        # TODO: Here goes the real component etc..
-          <h1>This is the instructions page!</h1>
-        </Route>
-        <Route path="/tool">
-          <h1>This is the tool page!</h1>
-        </Route>
-        <Route path="/publications">
-          <h1>This is the publications page!</h1>
-        </Route>
-        <Route path="/contacts">
-          <h1>This is the contact page!</h1>
-        </Route>
-        <Route path="/">
-          <h1>This is the home page!</h1>
-        </Route>
-      </Switch>
-    </Router>
+    <SideBar>
+      <ul>
+        <LinkItem>
+          <NavLink to="/">Home</NavLink>
+        </LinkItem>
+        <LinkItem>
+          <NavLink to="/instructions">Instructions</NavLink>
+        </LinkItem>
+        <LinkItem>
+          <NavLink to="/tool">Tool</NavLink>
+        </LinkItem>
+        <LinkItem>
+          <NavLink to="/publications">Publications</NavLink>
+        </LinkItem>
+        <LinkItem>
+          <NavLink to="/contact">Contact</NavLink>
+        </LinkItem>
+      </ul>
+    </SideBar>
   );
 };
 
