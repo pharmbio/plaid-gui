1f7b6eb4289a97d4c5bd896210709ac5f391947a
Travis Persson <travispersson93@gmail.com>
2020-12-06 15:35:36 +0100

    path fix - added exempel return to client

--- api/src/app.py
+++ api/src/app.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from models.minizinc_model import MinizincModel
 from utils.utils import output_to_json
 from flask import Flask, jsonify, request
@@ -10,8 +11,9 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/test_result")
 def test_result_to_json():
-    mz = MinizincModel("src/models/plate-design.mzn", "gecode")
-    mz.populate_instance(dzn_file_path="src/plate_design/dzn_examples/pl-example01.dzn")
+    #print(os.path.abspath(__file__))
+    mz = MinizincModel("./src/plate-design.mzn", "gecode")
+    mz.populate_instance(dzn_file_path="/src/plate_design/dzn_examples/pl-example01.dzn")
     result = mz.solve_instance()
     j_res = output_to_json(result)
     return j_res
@@ -24,7 +26,12 @@ def test_plaid():
     if request.method == 'POST':
         data = request.get_json()
         print(data, file=sys.stderr)
-    return data
+    #return data
+    mz = MinizincModel("./src/plate-design.mzn", "gecode")
+    mz.populate_instance(dzn_file_path="/src/plate_design/dzn_examples/pl-example01.dzn")
+    result = mz.solve_instance()
+    j_res = output_to_json(result)
+    return j_res
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=os.getenv("PORT"))
