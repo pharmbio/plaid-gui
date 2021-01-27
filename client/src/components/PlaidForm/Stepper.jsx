3d17f65b5d5655b4fc799a41ec436cd2c69300c9
Travis Persson <travispersson93@gmail.com>
2021-01-27 19:59:28 +0100

    redesign next/prev buttons

--- client/src/components/PlaidForm/Stepper.jsx
+++ client/src/components/PlaidForm/Stepper.jsx
@@ -2,17 +2,20 @@ import React, { useState } from "react";
 import HorizontalStepper from "./HorizontalStepper";
 import { Formik, Form } from "formik";
 import styled from "styled-components";
+import NextButton from "./Buttons/NextButton";
+import PrevButton from "./Buttons/PrevButton";
+
 const StyledForm = styled(Form)`
   display: flex;
   flex-direction: column;
-  justify-content:space-between;
+  justify-content: space-between;
   margin: auto;
   height: 60vh;
   width: 40vw;
 `;
 const ErrorNotice = styled.div`
   align-self: center;
-  display: ${(props) => props.error ? 'flex' : 'none'};
+  display: ${(props) => (props.error ? "flex" : "none")};
   flex-direction: row;
   color: black;
   justify-content: center;
@@ -22,103 +25,75 @@ const ErrorNotice = styled.div`
   margin: auto;
 `;
 
-const StyledInputContainer = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  flex-direction: column;
-  align-items: start;
-`;
-const StyledNextButton = styled.button`
-  margin-left:auto;
-  background: #0069eb;
-  color: #fff;
-  border: none;
-  border-radius: 0px;
-  font-size: 16px;
-  padding: 12px 26px;
-  text-decoration: none;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-`;
-const StyledPrevButton = styled.button`
-  background: #a6a6a6;
-  color: #fff;
-  border: none;
-  border-radius: 0px;
-  font-size: 16px;
-  padding: 12px 26px;
-  text-decoration: none;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-`;
 const StyledButtonContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
-  margin: 15px;
+  justify-content: flex-end;
+  margin: 10px;
 `;
+
 const Stepper = ({ children, ...props }) => {
-    const childrenArray = React.Children.toArray(children);
-    const [step, setStep] = useState(0);
-    const [errorState, setErrorState] = useState(true);
-    //Child to display on current step. 0 would be immediate child.
-    const currentChild = childrenArray[step];
-    function isLast() {
-        return step === childrenArray.length - 1;
-    }
- /* TODO: onChange eller onClick? Hur hanterar jag då onClick validering, väntar på svar och avgör sedan om vi går next eller ej?
+  const childrenArray = React.Children.toArray(children);
+  const [step, setStep] = useState(0);
+  const [errorState, setErrorState] = useState(true);
+  //Child to display on current step. 0 would be immediate child.
+  const currentChild = childrenArray[step];
+  function isLast() {
+    return step === childrenArray.length - 1;
+  }
+  /* TODO: onChange eller onClick? Hur hanterar jag då onClick validering, väntar på svar och avgör sedan om vi går next eller ej?
           om jag väljer onChange, hur kan vi stoppa valideringen från att köra på de tomma fälten innan man använt toolen? */
-    function handleNext() {
-        let errors = props.formUtils.onClick(); 
-        if(noErrors(errors)){
-            setStep(step + 1);
-        };
+  function handleNext() {
+    //let errors = props.formUtils.onClick();
+    //console.log(errors);
+    //if (noErrors(errors)) {
+    setStep(step + 1);
+    //}
+  }
+
+  function noErrors(errorObj) {
+    for (var key in errorObj) {
+      if (errorObj[key] != null) {
+        return false;
+      }
     }
+    return true;
+  }
 
-    function noErrors(errorObj) {
-        for (var key in errorObj) {
-            if (errorObj[key] != null) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    return (
-        <Formik
-            {...props}
-            initialValues={props.initialValues}
-        ><>
-                <ErrorNotice error={props.flightState['responseError']}> {props.flightState['responseError'] ? props.responseError : null}</ErrorNotice>
-                <StyledForm>
-                    <HorizontalStepper currentStep={step} steps={childrenArray} />
-                    <StyledInputContainer>{currentChild}</StyledInputContainer>
-                    <StyledButtonContainer>
-                        {step > 0 ? (
-                            <StyledPrevButton type="button" onClick={() => setStep(step - 1)}>
-                                Previous
-                            </StyledPrevButton>
-                        ) : null}
-                        <StyledNextButton
-                            type="button"
-                            onClick={
-                                isLast()
-                                    ? () =>
-                                        props.postForm(
-                                            props.initialValues,
-                                            props.setResponseError,
-                                            props.setFlightState,
-                                            props.flightState,
-                                            props.setData
-                                        )
-                                    : () => handleNext()
-
-                            }
-                        >
-                            {isLast() ? "Submit" : "Next"}
-                        </StyledNextButton>
-                    </StyledButtonContainer>
-                </StyledForm>
-            </>
-        </Formik>
-    );
+  return (
+    <Formik {...props} initialValues={props.initialValues}>
+      <>
+        <ErrorNotice error={props.flightState["responseError"]}>
+          {" "}
+          {props.flightState["responseError"] ? props.responseError : null}
+        </ErrorNotice>
+        <StyledForm>
+          <HorizontalStepper currentStep={step} steps={childrenArray} />
+          {currentChild}
+          <StyledButtonContainer>
+            {step > 0 ? (
+              <PrevButton type="button" onClick={() => setStep(step - 1)} />
+            ) : null}
+            <NextButton
+              type="button"
+              isLast={isLast()}
+              onClick={
+                isLast()
+                  ? () =>
+                      props.postForm(
+                        props.initialValues,
+                        props.setResponseError,
+                        props.setFlightState,
+                        props.flightState,
+                        props.setData
+                      )
+                  : () => handleNext()
+              }
+            />
+          </StyledButtonContainer>
+        </StyledForm>
+      </>
+    </Formik>
+  );
 };
 export default Stepper;
