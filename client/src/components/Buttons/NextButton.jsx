bb42732eae60a0d0192c04fa9ead89e00a014363
MarkusLucero <markuslucero@hotmail.com>
2021-03-04 20:38:04 +0100

    fixed minor fixes 1, 2, 3

--- client/src/components/Buttons/NextButton.jsx
+++ client/src/components/Buttons/NextButton.jsx
@@ -3,21 +3,23 @@ import styled from "styled-components";
 /* import { BiRightArrowAlt } from "react-icons/bi"; */
 
 const StyledButton = styled.button`
-  background: none;
-  color: inherit;
-  border: none;
-  padding: 10px;
-  font: inherit;
+  font-family: Whitney, "Open Sans", Helvetica, sans-serif;
+  background-color: #5096FF;
+  border: 2px solid #5096FF;
+  color: #fff;
+  font-weight: 400;
+  font-size: 16pt;
+  border-radius: 25px;
   cursor: pointer;
-  outline: inherit;
-  margin: 15px;
-  border-radius: 7px;
-  border: 1px solid #ccc;
+  height: 45px;
+  width: 130px;
+  margin-left: 20px;
+  box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.2);
   &:hover {
     outline: none;
     border: 1px solid #5096ff;
   }
-`;
+`
 
 const NextButton = (props) => {
   return (
