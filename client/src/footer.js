84c7be9b98ccb6dd7f4c351b01a9c9f8c9a89d4d
Andreina Francisco <maria.andreina.francisco@gmail.com>
2022-11-30 17:26:36 +0100

    Create footer.js

---
+++ client/src/footer.js
@@ -0,0 +1,66 @@
+import React from "react";
+import styled from "styled-components";
+import essence from "./assets/logos/essence_rgb.jpg";
+import haste from "./assets/logos/haste-w200.png";
+import pharmbio from "./assets/logos/pharmbio-logo.png";
+import scilife from "./assets/logos/scilife-logo.jpg";
+import uu from "./assets/logos/uu_logo.png";
+
+const StyledFooter = styled.footer`
+    margin-top: 1rem;
+    padding: 1rem;
+    position: relative;
+    bottom: 0;
+    left: 0;
+    height: 100px;
+    width: 100%;
+`;
+
+const StyledScaledImage = styled.img.attrs((props) => ({
+    src: props.src,
+    alt: props.alt,
+    width: props.width,
+    height: props.height,
+  }))`
+    display: inline-block;
+    vertical-align: top;
+    padding: 0px 20px;
+  `;
+
+const Footer = () => (
+  <div className="footer">
+    <StyledFooter>
+    <StyledScaledImage
+            src={pharmbio}
+            alt="Pharmbio"
+            height="90%"
+          />
+
+    <StyledScaledImage
+            src={essence}
+            alt="eSSENCE"
+            height="100%"
+          />
+    
+    <StyledScaledImage
+            src={haste}
+            alt="HASTE"
+            height="100%"
+          />
+
+    <StyledScaledImage
+            src={scilife}
+            alt="scilife"
+            height="100%"
+          />
+
+    <StyledScaledImage
+            src={uu}
+            alt="Uppsala University"
+            height="100%"
+          />
+   </StyledFooter>
+  </div>
+);
+
+export default Footer; 
